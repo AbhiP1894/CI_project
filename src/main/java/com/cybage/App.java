de123e2801f242a4be489ec06949a356feca97bb
Abhijeet Hemchandra Patil <97868310+AbhiP1894@users.noreply.github.com>
2024-10-15 14:45:01 +0530

    Update App.java

--- src/main/java/com/cybage/App.java
+++ src/main/java/com/cybage/App.java
@@ -6,29 +6,44 @@ package com.cybage;
  */
 public class App 
 {
-	public boolean isPalindrome(String input) {
-		if(input==null) {
-			throw new IllegalArgumentException("Input shouldnt be null");
-		}
-		if(input.equals(reverse(input))) {
-			return true;
-		}
-		else {
-			return false;
-		}
-		
-		
-	}
-//    public static void main( String[] args )
-//    {
-//        System.out.println( "Hello World!" );
-//    }
+    public boolean isPalindrome(String input) {
+        if (input == null) {
+            throw new IllegalArgumentException("Input shouldn't be null");
+        }
+        return input.equals(reverse(input));
+    }
 
-	private String reverse(String input) {
-		String rev= "";
-		for(int i=input.length() - 1; i>=0; i--) {
-			rev=rev + input.charAt(i);
-		}
-		return rev;
-	}
+    // Calculator methods
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public double divide(int a, int b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Cannot divide by zero");
+        }
+        return (double) a / b;
+    }
+
+    private String reverse(String input) {
+        String rev = "";
+        for (int i = input.length() - 1; i >= 0; i--) {
+            rev = rev + input.charAt(i);
+        }
+        return rev;
+    }
+    
+    // Uncomment if needed for testing
+    // public static void main(String[] args) {
+    //     System.out.println("Hello World!");
+    // }
 }
